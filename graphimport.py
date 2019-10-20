c3ff0541a7c8456da7c42717794448dc07f5e6f4
Tara L Andrews <tla@mit.edu>
2019-10-20 23:09:41 +0200

    restructure into subroutines, prepare to ingest more sorts of factoid

--- graphimport.py
+++ graphimport.py
@@ -5,13 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from neo4j import GraphDatabase
 
-engine = create_engine('mysql+pymysql://' + config.dbstring)
-smaker = sessionmaker(bind=engine)
-sqlsession = smaker()
-
 
 # Make our list of sources / authorities
 def get_authmap():
+    """Return a map of source -> authority (i.e. interpreter of that source) for the PBW data"""
     mj = 'Michael Jeffreys'
     tp = 'Tassos Papacostas'
     ta = 'Tara Andrews'
@@ -156,121 +153,166 @@ def get_authmap():
 
 
 def escape_text(t):
+    """Escape any single quotes in strings that need to go into Neo4J properties"""
     return t.replace("'", "\\'")
 
 
-# Get our authority map
-authMap = get_authmap()
-# Connect to the graph DB
-driver = GraphDatabase.driver(config.graphuri, auth=(config.graphuser, config.passphrase))
-
-# Get a list of people whose floruit matches our needs
-floruit = r'[ML] XI(?!I)'
-late11s = [x for x in sqlsession.query(pbw.Person).all() if re.match(floruit, x.floruit) and len(x.factoids) > 0]
-print("Found %d relevant people" % len(late11s))
-
-# Now start adding them to the graph database.
-# Make our agent, who is PBW
-with driver.session() as session:
-    exists = session.run("MERGE (a:Agent {identifier:'PBW'}) return a").single()
-    agent = exists['a']
-
-# Now we are going to derive a bunch of assertions from the PBW database.
-# For each of our people, they are a PBWPerson entity.
-# The Person table has assertions about their descriptions and about their original-language names.
-# First some setup of common objects from the Person table.
-# - sex
-with driver.session() as session:
-    sexes = session.run(
-        "MERGE (Female:Sex {value:'female'}) MERGE (Male:Sex {value:'male'}) MERGE (Eunuch:Sex {value:'eunuch'}) "
-        "MERGE (Unknown:Sex {value:'unknown'}) RETURN Female, Male, Eunuch, Unknown").single()
-    sex_predicate = session.run("MERGE (p:Predicate {label:'is_of_sex'}) RETURN p").single()['p']
-    # - short description
-    description_predicate = session.run("MERGE (p:Predicate {label:'has_description'}) RETURN p").single()['p']
-    # - identifier in the Person record
-    identified_predicate = session.run("MERGE (p:Predicate {label:'identified_as'}) RETURN p").single()['p']
-    # - Make the predicates for the different sorts of factoids
-    factoid_predicates = {}
-    for r in sqlsession.query(pbw.FactoidType).all():
-        label = r.typeName.lower().replace('/', '_').replace(' ', '_')
-        if label not in ['uncertain_ident', 'alternative_name', 'eunuchs']:
-            label = 'in_' + label if label == 'narrative' else 'has_' + label
-            fpred = session.run("MERGE (p:Predicate {label:'%s'}) RETURN p" % label).single()['p']
-            factoid_predicates[r.typeName] = fpred
+def collect_person_records(sqlsession):
+    # Get a list of people whose floruit matches our needs
+    floruit = r'[ML] XI(?!I)'
+    relevant = [x for x in sqlsession.query(pbw.Person).all() if re.match(floruit, x.floruit) and len(x.factoids) > 0]
+    print("Found %d relevant people" % len(relevant))
+    return relevant
 
 
-for person in late11s:
-    # Skip the Anonymi for now
-    if person.name is 'Anonymi':
-        continue
-    # Create or find the person node
-    print("Making/finding node for person %s %d" % (person.name, person.mdbCode))
-    nodelookup = "MERGE (p:PBWPerson {name:'%s', code:%d}) RETURN p" % (person.name, person.mdbCode)
-    with driver.session() as session:
-        graphPerson = session.run(nodelookup).single()['p']
-        # Add the assertions that are in the direct person record:
-        # - sex
-        uncertain = False
-        ourSex = person.sex
-        if ourSex == 'Mixed':  # we have already excluded Anonymi
-            ourSex = 'Unknown'
-        elif ourSex == '(Unspecified)':
-            ourSex = 'Unknown'
-        elif ourSex == 'Eunuch (Probable)':
-            ourSex = 'Eunuch'
-            uncertain = True
-        if uncertain:
-            assertionProps = ' {uncertain:true}'
-        else:
-            assertionProps = ''
-        print("...setting sex to %s%s" % (ourSex, " (maybe)" if uncertain else ""))
-        sexassertion = "MATCH (p), (sp), (s), (pbw) WHERE id(p) = %d AND id(sp) = %d AND id(s) = %d AND id(pbw) = %d " \
-                       "MERGE (sp)<-[:PREDICATE]-(a:Assertion%s)-[:SUBJECT]->(p) MERGE (pbw)<-[:AUTHORITY]-(a)-[" \
-                       ":OBJECT]->(s) RETURN a" % (graphPerson.id, sex_predicate.id, sexes[ourSex].id, agent.id,
-                                                   assertionProps)
-        session.run(sexassertion)
+def setup_constants(sqlsession, graphdriver):
+    """Set up the necessary object and predicate nodes that will be shared by the individual records"""
+    with graphdriver.session() as session:
+        # Make our anonymous agent PBW for the un-sourced information
+        generic_agent = session.run("MERGE (a:Agent {identifier:'PBW'}) return a").single()['a']
+        # Set up the distinct sexes in the database
+        pbw_sexes = session.run(
+            "MERGE (Female:Sex {value:'female'}) MERGE (Male:Sex {value:'male'}) MERGE (Eunuch:Sex {value:'eunuch'}) "
+            "RETURN Female, Male, Eunuch").single()
+        # Set up the is_of_sex predicate
+        sex_predicate = session.run("MERGE (p:Predicate {label:'is_of_sex'}) RETURN p").single()['p']
+        # Set up the has_disambiguation predicate
+        disambig_predicate = session.run("MERGE (p:Predicate {label:'has_disambiguation'}) RETURN p").single()['p']
+        # Set up the identified_as predicate
+        identified_predicate = session.run("MERGE (p:Predicate {label:'identified_as'}) RETURN p").single()['p']
+        # Collect these into a dictionary
+        factoid_predicates = {
+            'sex': sex_predicate,
+            'disambiguation': disambig_predicate,
+            'identified': identified_predicate
+        }
+        # Create and collect predicates for the explicit factoid categories
+        for r in sqlsession.query(pbw.FactoidType).all():
+            label = r.typeName.lower().replace('/', '_').replace(' ', '_')
+            if label not in ['uncertain_ident', 'alternative_name', 'eunuchs', '(unspecified)']:
+                label = 'in_' + label if label == 'narrative' else 'has_' + label
+                fpred = session.run("MERGE (p:Predicate {label:'%s'}) RETURN p" % label).single()['p']
+                factoid_predicates[r.typeName] = fpred
+    # Return our agent, our sex objects, and our predicates
+    return generic_agent, pbw_sexes, factoid_predicates
 
-        # - description
-        print("...setting description to %s" % person.descName)
-        descassertion = "MATCH (p), (dp), (pbw) WHERE id(p) = %d AND id(dp) = %d AND id(pbw) = %d MERGE (dp)<-[" \
-                        ":PREDICATE]-(a:Assertion)-[:SUBJECT]->(p) MERGE (pbw)<-[:AUTHORITY]-(a)-[:OBJECT]->(" \
-                        "d:Description {text:'%s'}) RETURN a" % (graphPerson.id, description_predicate.id, agent.id,
-                                                                 escape_text(person.descName))
-        session.run(descassertion)
 
-        # - name/identifier
-        print("...setting identifier to %s" % person.nameOL)
-        identassertion = "MATCH (p), (ip), (pbw) WHERE id(p) = %d AND id(ip) = %d AND id(pbw) = %d MERGE (ip)<-[" \
-                         ":PREDICATE]-(a:Assertion)-[:SUBJECT]->(p) MERGE (pbw)<-[:AUTHORITY]-(a)-[:OBJECT]->(" \
-                         "i:Identifier {text:'%s'}) RETURN a" % (graphPerson.id, identified_predicate.id, agent.id,
-                                                                 escape_text(person.nameOL))
-        session.run(identassertion)
+def get_object_node(session, factoid_type, factoid):
+    if factoid_type == "Dignity/Office":
+        if factoid.dignityOffice is None:
+            return None
+        return session.run("MERGE (do:DignityOffice {name:'%s', name_en:'%s'}) RETURN do"
+                           % (escape_text(factoid.dignityOffice.stdNameOL),
+                              escape_text(factoid.dignityOffice.stdName))).single()['do']
+    return None
 
-        # Now add the different sorts of factoids
-        for f in person.main_factoids('Dignity/Office'):
-            # The factoid has the same subject, a predicate "held_dignity_office", an object which is the office,
-            # a source from which the information comes, and an authority who interpreted the source.
-            if f.dignityOffice is None:
-                print("WARNING: Skipping un-factoided dignity on person %d (%s %d)"
-                      % (person.personKey, person.name, person.mdbCode))
-                continue
-            dignityNotes = ", notes:'%s'" % escape_text(f.notes) if f.notes else ""
-            dignityNode = session.run("MERGE (do:DignityOffice {name:'%s', name_en:'%s'%s}) RETURN do"
-                                      % (escape_text(f.dignityOffice.stdNameOL), escape_text(f.dignityOffice.stdName),
-                                         dignityNotes)).single()['do']
-            sourceNode = session.run("MERGE (src:Source {identifier:'%s', reference:'%s'}) RETURN src"
-                                     % (escape_text(f.source), escape_text(f.sourceRef))).single()['src']
-            factoidAuth = authMap.get(f.source, [])
-            # Make sure the assertion itself exists, and then add the source and authorities.
-            # This lets us have multiple sources making the same assertion.
-            doassertion = "MATCH (p), (dop), (do) WHERE id(p) = %d AND id(dop) = %d AND id(do) = %d MERGE (dop)<-[" \
-                          ":PREDICATE]-(a)-[:SUBJECT]->(p) MERGE (a)-[:OBJECT]->(do) RETURN a" \
-                          % (graphPerson.id, factoid_predicates['Dignity/Office'].id, dignityNode.id)
-            a = session.run(doassertion).single()['a']
-            # Now add the source
-            session.run("MATCH (a), (src) WHERE id(a) = %d AND id(src) = %d MERGE (a)-[:SOURCE]->(src)"
-                        % (a.id, sourceNode.id))
-            for auth in factoidAuth:
-                authNode = session.run("MERGE (agent:Agent {identifier:'%s'}) RETURN agent" % auth).single()['agent']
-                session.run("MATCH (a), (ag) WHERE id(a) = %d AND id(ag) = %d MERGE (a)-[:AUTHORITY]->(ag)"
-                            % (a.id, authNode.id))
+
+def process_persons(personlist, graphdriver, agent, sexes, predicates):
+    """Go through the relevant person records and process them for factoids"""
+    processed = 0
+    for person in personlist:
+        # Skip the Anonymi for now
+        if person.name is 'Anonymi':
+            continue
+        processed += 1
+        # Create or find the person node
+        print("Making/finding node for person %s %d" % (person.name, person.mdbCode))
+        nodelookup = "MERGE (p:PBWPerson {name:'%s', code:%d}) RETURN p" % (person.name, person.mdbCode)
+        with graphdriver.session() as session:
+            graph_person = session.run(nodelookup).single()['p']
+            # Add the assertions that are in the direct person record:
+            # - sex
+            for ftype in predicates:
+                predicate = predicates[ftype]
+                if ftype == 'sex':
+                    uncertain = False
+                    our_sex = person.sex
+                    if our_sex == 'Mixed':  # we have already excluded Anonymi
+                        our_sex = 'Unknown'
+                    elif our_sex == '(Unspecified)':
+                        our_sex = 'Unknown'
+                    elif our_sex == 'Eunuch (Probable)':
+                        our_sex = 'Eunuch'
+                        uncertain = True
+                    if uncertain:
+                        assertion_props = ' {uncertain:true}'
+                    else:
+                        assertion_props = ''
+                    if our_sex != "Unknown":
+                        print("...setting sex to %s%s" % (our_sex, " (maybe)" if uncertain else ""))
+                        sexassertion = "MATCH (p), (sp), (s), (pbw) WHERE id(p) = %d AND id(sp) = %d AND id(s) = %d " \
+                                       "AND id(pbw) = %d MERGE (sp)<-[:PREDICATE]-(a:Assertion%s)-[:SUBJECT]->(p) " \
+                                       "MERGE (pbw)<-[:AUTHORITY]-(a)-[:OBJECT]->(s) RETURN a" \
+                                       % (graph_person.id, predicate.id, sexes[our_sex].id, agent.id, assertion_props)
+                        session.run(sexassertion)
+                elif ftype == 'disambiguation':
+                    # - description / disambiguator
+                    print("...setting disambiguating description to %s" % person.descName)
+                    disamb_node = session.run("MERGE (d:Disambiguation {text:'%s'}) RETURN d"
+                                              % escape_text(person.descName)).single()['d']
+                    disassertion = "MATCH (p), (dp), (pbw), (d) WHERE id(p) = %d AND id(dp) = %d AND id(pbw) = %d AND "\
+                                   "id(d) = %d MERGE (dp)<-[:PREDICATE]-(a:Assertion)-[:SUBJECT]->(p) MERGE (pbw)<-[" \
+                                   ":AUTHORITY]-(a)-[:OBJECT]->(d) RETURN a" % (graph_person.id, predicate.id,
+                                                                                agent.id, disamb_node.id)
+                    session.run(disassertion)
+                elif ftype == 'identified':
+                    # - name/identifier
+                    print("...setting identifier to %s" % person.nameOL)
+                    ident_node = session.run("MERGE (i:Identifier {text:'%s'}) RETURN i").single()['i']
+                    identassertion = "MATCH (p), (ip), (pbw), (i) WHERE id(p) = %d AND id(ip) = %d AND id(pbw) = %d " \
+                                     "AND id(i) = %d MERGE (ip)<-[:PREDICATE]-(a:Assertion)-[:SUBJECT]->(p) MERGE (" \
+                                     "pbw)<-[:AUTHORITY]-(a)-[:OBJECT]->(i) RETURN a" \
+                                     % (graph_person.id, predicate.id, agent.id, ident_node.id)
+                    session.run(identassertion)
+                else:
+                    # Now add the different sorts of explicit factoids
+                    for f in person.main_factoids(ftype):
+                        # The factoid has the same subject, the predicate we created, some object (which will depend
+                        # on the factoid type), a source from which the information comes, and an authority who
+                        # interpreted the source. It might also have a qualifier in "notes".
+                        # First thing: collect the object in question.
+                        obj = get_object_node(session, ftype, f)
+                        if obj is None:
+                            print("Skipping un-factoided %s on person %d (%s %d)"
+                                  % (ftype, person.personKey, person.name, person.mdbCode))
+                            continue
+                        # Get the qualifier, if there is one
+                        qualifier = " {notes:'%s'}" % escape_text(f.notes) if f.notes else ""
+                        # Get the source and the authority
+                        source_node = session.run("MERGE (src:Source {identifier:'%s', reference:'%s'}) RETURN src"
+                                                  % (escape_text(f.source), escape_text(f.sourceRef))).single()['src']
+                        factoid_auth = auth_map.get(f.source, [])
+                        # Make sure the assertion itself exists, and then add the source and authorities.
+                        # This lets us have multiple sources making the same assertion, or multiple notes on
+                        # the assertion predicate.
+                        doassertion = "MATCH (p), (dop), (do) WHERE id(p) = %d AND id(dop) = %d AND id(do) = %d MERGE "\
+                                      "(do)<-[:OBJECT]-(a:Assertion)-[:SUBJECT]->(p) MERGE (a)-[:PREDICATE%s]->(dop) " \
+                                      "RETURN DISTINCT a" % (graph_person.id, predicate.id, obj.id, qualifier)
+                        a = session.run(doassertion).single()['a']
+                        # Now add the source
+                        session.run("MATCH (a), (src) WHERE id(a) = %d AND id(src) = %d MERGE (a)-[:SOURCE]->(src)"
+                                    % (a.id, source_node.id))
+                        for auth in factoid_auth:
+                            auth_node = session.run("MERGE (ag:Agent {identifier:'%s'}) RETURN ag"
+                                                    % auth).single()['ag']
+                            session.run("MATCH (a), (ag) WHERE id(a) = %d AND id(ag) = %d MERGE (a)-[:AUTHORITY]->(ag)"
+                                        % (a.id, auth_node.id))
+
+    print("Processed %d person records." % processed)
+
+
+# If we are running as main, execute the script
+if __name__ == '__main__':
+    # Connect to the SQL DB
+    engine = create_engine('mysql+pymysql://' + config.dbstring)
+    smaker = sessionmaker(bind=engine)
+    mysqlsession = smaker()
+    # Connect to the graph DB
+    driver = GraphDatabase.driver(config.graphuri, auth=(config.graphuser, config.passphrase))
+    # Get our authority map
+    auth_map = get_authmap()
+    # Make / retrieve the global nodes
+    (pbwagent, pbwsexes, pbwpredicates) = setup_constants(mysqlsession, driver)
+    # Process the person records
+    process_persons(collect_person_records(mysqlsession), driver, pbwagent, pbwsexes, pbwpredicates)
+    print("Done!")
