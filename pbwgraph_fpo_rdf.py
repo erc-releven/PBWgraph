f1ca0b0150050ed733980e303351f8ce4f55f28e
Carla_Ebel <carla.ebel@oeaw.ac.at>
2022-11-30 15:00:17 +0100

    added labels, locations, language tags

--- pbwgraph_fpo_rdf.py
+++ pbwgraph_fpo_rdf.py
@@ -7,6 +7,21 @@ from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import urlencode
+import re
+from pbwgraph_config import create_engine_str
+from graphimportSTAR import get_authmap
+
+mj = 'Michael Jeffreys'
+tp = 'Tassos Papacostas'
+ta = 'Tara Andrews'
+jr = 'Judith Ryder'
+mw = 'Mary Whitby'
+wa = 'Wahid Amin'
+bs = 'Bruna Soravia'
+hm = 'Harry Munt'
+lo = 'Letizia Osti'
+cr = 'Charlotte Roueché'
+ok = 'Olga Karagiorgiou'
 
 
 pbwfpo=Namespace('http://example.com/PBW/FPO/test_v1/entity/')
@@ -37,6 +52,11 @@ owl=Namespace('http://www.w3.org/2002/07/owl#')
 """Defines OWL namespace."""
 frbroo=Namespace('http://www.ifla.org/files/assets/cataloguing/FRBRoo/frbroo_v_2.4.pdf#')
 """Defines frbroo namespace."""
+pleiades_place=Namespace('http://pleiades.stoa.org/places/')
+""""Namespace for places from Pleiades"""
+geonames_place=Namespace('http://www.geonames.org/')
+""""Namespace for places from Pleiades"""
+
 
 EventFactoidList = [4, 7]
 StateOfAffairsFactoid = [9, 11, 14]
@@ -49,6 +69,12 @@ def create_factoid_uris(a):
 # def create_person_uris(b):
 #     person_base_uri_str=f"{pbwfpo}factoid/{b.personKey}"
 #     return person_base_uri_str
+
+def create_rdflabel(engDesc):
+    if re.search('<PERSREF\sID=\\"(\d)*\\"\/>', engDesc):
+        engDesc = re.sub('"\/>',"",engDesc)
+        engDesc = re.sub('<PERSREF\sID=\\"', pbwfpo, engDesc)
+    return(engDesc)
 
 def create_factoid(engine, g, mysqlsession):
     #print(f"""pbw factoid: {fa.factoidKey}""")
@@ -65,8 +91,62 @@ def create_factoid(engine, g, mysqlsession):
         g.add((URIRef(f"{pbwfpo}SourceCitation/{a.factoidKey}"), RDF.type, fpocore.SourceCitation))
         g.add((URIRef(f"{pbwfpo}SourceCitation/{a.factoidKey}"), fpocore.hasPlaceInSource, Literal(a.sourceRef)))
         g.add((URIRef(f"{pbwfpo}SourceCitation/{a.factoidKey}"), fpocore.fromSource, URIRef(f"{pbwfpo}Source/{a.sourceKey}")))
+        rdf_engDesc = create_rdflabel(a.engDesc)
+        g.add((URIRef(factoid_base_uri_str), RDFS.label, Literal(rdf_engDesc, lang="en")))
+        #language tags according to ISO 639-1
+        if a.oLangKey == 5:
+            g.add((URIRef(factoid_base_uri_str), RDFS.label, Literal(a.origLDesc, lang="hy")))
+            #armenian original language tag
+        elif a.oLangKey == 4:
+            g.add((URIRef(factoid_base_uri_str), RDFS.label, Literal(a.origLDesc, lang="ar")))
+            #arabic original language tag
+        elif a.oLangKey == 3:
+            g.add((URIRef(factoid_base_uri_str), RDFS.label, Literal(a.origLDesc, lang="la")))
+            #latin original language tag
+        elif a.oLangKey == 2:
+            g.add((URIRef(factoid_base_uri_str), RDFS.label, Literal(a.origLDesc, lang="gr")))
+            #greek original language tag
+        else:
+            g.add((URIRef(factoid_base_uri_str), RDFS.label, Literal(a.origLDesc)))
+            #unspecified original language tag
     return g
 
+def locationref(engine, g, mysqlsession):
+    for h in mysqlsession.query(pbw.FactoidLocation):
+        factoid_base_uri_str = create_factoid_uris(h.factoidKey)
+        g.add ((URIRef(factoid_base_uri_str), fpocore.hasReference, URIRef((f"{pbwfpo}locationreference/{h.factoidKey}/{h.locationKey}"))))
+        g.add ((URIRef(f"{pbwfpo}locationreference/{h.factoidKey}/{h.locationKey}"), RDF.type, fpocore.LocationReference))
+        g.add ((URIRef(f"{pbwfpo}locationreference/{h.factoidKey}/{h.locationKey}"), fpocore.referencesLocation, URIRef(f"{pbwfpo}location/{h.locationKey}")))
+    return g
+
+def location(engine, g, mysqlsession):
+    for i in mysqlsession.query(pbw.Location):
+        g.add ((URIRef(f"{pbwfpo}location/{i.locationKey}"), RDF.type, fpocore.Location))
+        g.add ((URIRef(f"{pbwfpo}locationreference/{i.locationKey}"), RDFS.label, Literal(i.locName, lang="en")))
+        if i.oLangKey == 5:
+            g.add((URIRef(f"{pbwfpo}location/{í.locationKey}"), RDFS.label, Literal(i.locNameOL, lang="hy")))
+            #armenian original language tag
+        elif i.oLangKey == 4:
+            g.add((URIRef(f"{pbwfpo}location/{i.locationKey}"), RDFS.label, Literal(i.locNameOL, lang="ar")))
+            #arabic original language tag
+        elif i.oLangKey == 3:
+            g.add((URIRef(f"{pbwfpo}location/{i.locationKey}"), RDFS.label, Literal(i.locNameOL, lang="la")))
+            #latin original language tag
+        elif i.oLangKey == 2:
+            g.add((URIRef(f"{pbwfpo}location/{i.locationKey}"), RDFS.label, Literal(i.locNameOL, lang="gr")))
+            #greek original language tag
+        else:
+            g.add((URIRef(f"{pbwfpo}location/{i.locationKey}"), RDFS.label, Literal(i.locNameOL)))
+            #unspecified original language tag
+        if i.geonames_id != None:
+            g.add ((URIRef(f"{pbwfpo}location/{i.locationKey}"), owl.sameAs, URIRef(f"{geonames_place}{i.geonames_id}")))
+        else:
+            continue
+        if i.pleiades_id != None:
+            g.add ((URIRef(f"{pbwfpo}location/{i.locationKey}"), owl.sameAs, URIRef(f"{pleiades_place}{i.pleiades_id}")))
+        else:
+            continue
+    return g
 
 def create_source(engine, g, mysqlsession):
     for b in mysqlsession.query(pbw.Source).all():
@@ -87,7 +167,7 @@ def create_occupation_label(engine, g, mysqlsession):
     return g
 
 def create_person_uris(pk):
-    person_base_uri_str=f"{pbwfpo}person/{pk}"
+    person_base_uri_str=f"{pbwfpo}Person/{pk}"
     return person_base_uri_str
 
 
@@ -104,7 +184,7 @@ def create_person_reference (engine, g, mysqlsession):
 def create_person_labels(engine, g, mysqlsession):
     for f in mysqlsession.query(pbw.Person).all():
         person_uri = create_person_uris(f.personKey)
-        g.add((URIRef(person_uri), RDFS.label, Literal(f.descName)))
+        g.add((URIRef(person_uri), RDFS.label, Literal(f.descName, lang="en")))
     return g
 
 
@@ -141,14 +221,15 @@ def main(starttime, engine, mysqlsession):
     g = create_occupation_label(engine, g, mysqlsession)
     g = create_person_reference(engine, g, mysqlsession)
     g = create_person_labels(engine, g, mysqlsession)
+    g = locationref(engine, g, mysqlsession)
+    g = location(engine, g, mysqlsession)
     pbw_fpo = serializeto_ttl(g)
 
 
-# If we are running as main, execute the script
 if __name__ == '__main__':
     # Connect to the SQL DB
     starttime = datetime.now()
-    engine = create_engine('mysql+pymysql://cve:Avi1995@127.0.0.1:3306/pbw2022')
+    engine = create_engine(create_engine_str)
     smaker = sessionmaker(bind=engine)
     mysqlsession = smaker()
     main(starttime, engine, mysqlsession)
