a5bf70e8478e0b7049e5224e7dd3bd7da38304bc
Tara L Andrews <tla@mit.edu>
2026-02-18 14:49:09 +0100

    add lupl's URI generator

--- RELEVEN/PBWstarConstants.py
+++ RELEVEN/PBWstarConstants.py
@@ -3,6 +3,7 @@ import re
 import RELEVEN.PBWSources
 import sys
 from datetime import datetime
+from hashlib import sha256
 from os.path import join, dirname, basename
 from rdflib import Graph, URIRef, Literal, Namespace, OWL, RDF, RDFS, XSD
 from rdflib.query import Result
@@ -666,3 +667,39 @@ GROUP BY ?egroup HAVING (COUNT(?member) = {len(members)})
             self.graph.update(sparql_update)
         else:
             print("No new assertions created on this run.")
+
+
+# A donation from lupl
+#
+#     Persons: "Identifier / Service"
+#     Places: "Reference name"
+#     Controlled Vocabulary Terms: "Column name / type term"
+
+class URIConstructor:
+    """Namespaced URI constructor.
+
+    The URIConstructor class is initialized given a namespace.
+    Calls to the initialized object will construct rdflib.URIRefs for that namespace.
+
+    If a hash_value argument of type str | bytes is provided, the URIRef
+    will be generated with the sha256 hash of the hash_value argument as component;
+    else a URIRef with a unique component will be generated using UUID4.
+    """
+
+    def __init__(
+        self,
+        namespace: str,
+    ) -> None:
+        self.namespace = Namespace(namespace)
+
+    def __call__(self, hash_value: str | bytes | None = None) -> URIRef:
+        if hash_value is None:
+            segment = str(uuid4())
+        else:
+            if isinstance(hash_value, str):
+                hash_value = hash_value.encode("utf8")
+
+            digest = sha256(hash_value).hexdigest()
+            segment = digest[:36]
+
+        return self.namespace[segment]
